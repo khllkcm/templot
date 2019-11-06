d42107961e1228f62ad32950a306f72fbf95953a
khllkcm <khalilqacem@gmail.com>
2019-11-06 21:08:55 +0100

    Exported functions

---
+++ templot/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.1"
+__author__ = "Khalil Kacem"
+
+from .plot_aggregated_map import plot_aggregated_map  # noqa: F401
+from .utils import add_regions  # noqa: F401
+from .utils import download_irep  # noqa: F401
