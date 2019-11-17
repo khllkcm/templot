5f5c1445f37767bc046db2557ac651754118eeda
hamdi <43981118+hamdi@users.noreply.github.com>
2019-11-18 00:16:00 +0100

    Update __init__.py

--- templot/__init__.py
+++ templot/__init__.py
@@ -4,5 +4,7 @@ __author__ = "Khalil Kacem"
 from .plot_aggregated_map import plot_aggregated_map  # noqa: F401
 from .plot_polar_bar_evolution import plot_polar_bar_evolution  # noqa: F401
 from .plot_polar_bar_evolution_interactive import plot_polar_bar_evolution_interactive  # noqa: F401
+from .plot_top10_barchart import plot_top10_barchart  # noqa: F401
+from .plot_polar_bar_evolution import plot_polar_bar_evolution  # noqa: F401
 from .utils import add_regions  # noqa: F401
 from .utils import download_irep  # noqa: F401
